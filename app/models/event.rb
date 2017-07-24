97727796be44d67f373cc40ece7ccdef2c17c4a5
hanhan <hanhan_smith@hotmail.com>
2017-07-25 00:17:38 +0800

    完成票种添加

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,8 +1,11 @@
 class Event < ApplicationRecord
+  has_many :tickets, :dependent => :destroy
+  accepts_nested_attributes_for :tickets, :allow_destroy => true, :reject_if => :all_blank
+
   STATUS = ["draft", "public", "private"]
   validates_inclusion_of :status, :in => STATUS
 
-  validates_presence_of :name
+  #validates_presence_of :name
 
   validates_presence_of :name, :friendly_id
 
