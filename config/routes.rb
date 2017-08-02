79c04b8910f1a4398a60334e7473669ffe1cc5dc
hanhan <hanhan_smith@hotmail.com>
2017-08-02 22:48:46 +0800

    完成步骤表单

--- config/routes.rb
+++ config/routes.rb
@@ -2,14 +2,23 @@ Rails.application.routes.draw do
 
   devise_for :users
 
-  resources :events
+  resources :events do
+    resources :registrations do
+      member do
+        get "steps/2" => "registrations#step2", :as => :step2
+        patch "steps/2/update" => "registrations#step2_update", :as => :update_step2
+        get "steps/3" => "registrations#step3", :as => :step3
+        patch "steps/3/update" => "registrations#step3_update", :as => :update_step3
+      end
+    end
+  end
 
   namespace :admin do
     root "events#index"
 
     resources :events do
       resources :tickets, :controller => "event_tickets"
-      
+
         member do
           post :reorder
         end
